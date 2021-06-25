6672b00b6779b357eb70d2ec71e9260ad19d996f
isamu_yonezawa <yonezawa.isamu.hal@gmail.com>
2021-06-25 15:24:32 +0900

    secound

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->primary('id')->autoIncrement();
+            $table->srting('name',100);
+            $table->string('password',255)->notnull();
+            $table->string('mail')->nullable();
+            $table->int('authority',1);
+            $table->timestamp('created_at')->default();
+            $table->timestamp('deleted_at')->default();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//トップページ
+Route::get('/', 'AFSmainController@index');
