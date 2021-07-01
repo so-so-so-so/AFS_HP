8e4303a30d3ade86d7155e782cef9823175a232a
isamu_yonezawa <yonezawa.isamu.hal@gmail.com>
2021-07-01 14:48:42 +0900

    大曾根へ

--- app/Http/Controllers/AFSmainController.php
+++ app/Http/Controllers/AFSmainController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use app\Models\News;
 
 class AFSmainController extends Controller
 {
     public function index(){
-        return view('index');
+        $news = DB::select('select title,content,news_category,image_path,video_url FROM contents');
+        $data = ['news'=>$news];
+        return view('index',$data);
     }
 }
---
+++ database/seeds/contents_category.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class contents_category extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('')
+    }
+}
